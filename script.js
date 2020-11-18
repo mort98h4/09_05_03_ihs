a258c9987db609236ffd66a2e9891557da8d67c1
Morten Gross <morten@Mortens-MacBook-Air.local>
2020-11-18 10:03:18 +0100

    global menu DONE

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 "use strict"
 
+// JavaScript der lytter efter om der bliver scrollet på siden
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 400;
+
+    // Tilføjer/fjerner class'en der ændrer farve på menuen
+    header.classList.toggle('scrolling_active', windowPosition);
+})
+
 // JavaScript der styrer burgermenu
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav_links');
@@ -10,6 +19,7 @@ burger.addEventListener("click", () => {
     nav.classList.toggle("nav_active");
     // Tilføjer/fjerner "animation" på burgermenu
     burger.classList.toggle("toggle");
+    document.querySelector("header").classList.add("scrolling_active");
 });
 
 document.querySelector("#logo").addEventListener("click", () => {
